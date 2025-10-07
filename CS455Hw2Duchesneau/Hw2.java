1ac9bfce5bca578fc5e6ba431fa193cb5aded693
Kaleb <kalebduchesneau@gmail.com>
2025-10-06 22:14:44 -0500

    working on it

--- CS455Hw2Duchesneau/Hw2.java
+++ CS455Hw2Duchesneau/Hw2.java
@@ -30,8 +30,8 @@ public class Hw2 {
         final W w = new W();
 
         scene.addPosition(new Position(p, "p1", new Vector(0, 0, -1.5)));
-        scene.addPosition(new Position(p, "p2", new Vector(1, 0, -1.5)));
-        scene.addPosition(new Position(p, "p3", new Vector(2, 0, -1.5)));
+        scene.addPosition(new Position(n, "p2", new Vector(1, 0, -1.5)));
+        scene.addPosition(new Position(w, "p3", new Vector(2, 0, -1.5)));
 
         // If you need to, print the Scene data structure to the console.
         // This can help you check that your models and scene make sense.
@@ -52,8 +52,47 @@ public class Hw2 {
             Pipeline.render(scene, fb);
             fb.dumpFB2File(String.format("Hw2_frame%03d.ppm", i));
 
-            // Update each Model's location.
+            Vector vP = scene.getPosition(0).getTranslation();
+            Vector vN = scene.getPosition(1).getTranslation();
+            Vector vW = scene.getPosition(2).getTranslation();
+            double pX = vP.x, pY = vP.y, pZ = vP.z;
+            double nX = vN.x, nY = vN.y, nZ = vN.z;
+            double wX = vW.x, wY = vW.y, wZ = vW.z;
 
+            // Update each Model's location.
+            if (i < 25) {
+                pZ -=  0.08;
+            } else if (i < 75) {
+                nX -= 0.02;
+                wX -= 0.02;
+                pX += 0.04;
+            } else if (i < 100) {
+                pZ +=  0.08;
+            } else if (i < 125) {
+                nY += 0.04;
+            } else if (i < 175) {
+                nX += 0.04;
+                wX -= 0.02;
+                pX -= 0.02;
+            } else if (i < 200) {
+                nY -= 0.04;
+            } else if (i < 225) {
+                pY -= 0.04;
+                nY -= 0.04;
+            } else if (i < 250) {
+                wX += 0.04;
+                pX -= 0.04;
+            } else if (i < 275) {
+                wX += 0.04;
+                nX -= 0.04;
+            } else {
+                pY += 0.04;
+                nY += 0.04;
+            }
+
+            scene.setPosition(0, new Position(p, "p1", vP.plus(new Vector(pX, pY, pZ))));
+            scene.setPosition(1, new Position(n, "p2", vN.plus(new Vector(nX, nY, nZ))));
+            scene.setPosition(2, new Position(w, "p3", vW.plus(new Vector(wX, wY, wZ))));
         }
     }
 }
