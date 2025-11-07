1736847c684c186689fe033c2098f4acf24bbaf4
Kaleb Duchesneau <kalebduchesneau@gmail.com>
2025-11-06 20:16:57 -0600

    organized the files and removed redundant ones

--- CS455Hw2Duchesneau/ImageMagick-7.1.2-3-portable-Q8-x64.cmd
+++
@@ -1,9 +0,0 @@
-::
-:: This command file assumes that you have created
-:: the folder C:\ImageMagick-7.1.1-39-portable-Q8-x64
-::
-pushd  C:\ImageMagick-7.1.2-3-portable-Q8-x64
-set PATH=%CD%;%PATH%
-popd
-magick.exe  -delay 1x30  -loop 0  Hw2_frame*.ppm  animation.gif
-pause
--- CS455Hw2Duchesneau/build_&_run_client.cmd
+++
@@ -1,3 +0,0 @@
-javac -g -Xlint -Xdiags:verbose  -cp .;renderer_1.jar  %1
-java                             -cp .;renderer_1.jar  %~n1
-pause
